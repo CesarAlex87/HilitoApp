491079530e627e628b4be648d8441d6a694fe818
JimmyJP25 <jimmypaez74@gmail.com>
2023-08-25 16:13:26 -0500

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioSessionComponent } from './components/inicio-session/inicio-session.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioSessionComponent
   ],
   imports: [
     BrowserModule,
