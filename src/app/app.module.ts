1399cbe5805cf4cfb28e008d8ef60bf07372fd84
JimmyJP25 <jimmypaez74@gmail.com>
2023-08-28 11:41:04 -0500

    Avances de login v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { InicioSessionComponent } from './components/inicio-session/inicio-session.component';
-=======
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { FormsModule } from '@angular/forms';
@@ -15,18 +12,13 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
->>>>>>> 34d9d0a54c46766df2d08bc9ad6fb85657c18874
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    InicioSessionComponent
-=======
     LoginComponent,
     RegisterComponent,
     HomeComponent,
->>>>>>> 34d9d0a54c46766df2d08bc9ad6fb85657c18874
   ],
   imports: [BrowserModule, RouterModule.forRoot(routes), FormsModule],
   exports: [RouterModule],
